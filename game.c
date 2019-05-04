d1caaf3e28959af1efec261c47a2486849be8a69
Benjamin Hagan <henbagan@gmail.com>
2019-05-03 17:22:53 -0700

    works with malloc and reading from file

--- game.c
+++ game.c
@@ -194,7 +194,7 @@ void gameEntry(uint16_t * g, uint16_t * newg, uint16_t size, uint16_t iterations
 	uint16_t (*newgrid)[size] = (uint16_t (*)[size]) newg;
 	Coord coord;
 	for (i = 0; i < size; i++) {
-		printf("\n");
+		// printf("\n");
 		for (j = 0; j < size; j++) {
 			// printf("%d", grid[i][j]);
 			// printf("%d ", grid[i][j]);
@@ -239,52 +239,52 @@ int main(int argc, char ** argv) {
 	FILE *f = fopen(argv[1], "r");
 	fscanf (f, "%hd", &sz);    
 	printf ("size = %d, build table ", sz);
-	uint16_t **buff_0 = (uint16_t**)malloc(sizeof(uint16_t*)*sz);
-	uint16_t **buff_1 = (uint16_t**)malloc(sizeof(uint16_t*)*sz);
+	// uint16_t **buff_0 = (uint16_t**)malloc(sizeof(uint16_t*)*sz);
+	// uint16_t **buff_1 = (uint16_t**)malloc(sizeof(uint16_t*)*sz);
 	uint16_t *buff_x = (uint16_t*)malloc(sizeof(uint16_t)*sz*sz);
-	// uint16_t *buff_0 = (uint16_t*)malloc(sizeof(uint16_t)*sz*sz);
-	// uint16_t *buff_1 = (uint16_t*)malloc(sizeof(uint16_t)*sz*sz);
+	uint16_t *buff_0 = (uint16_t*)malloc(sizeof(uint16_t)*sz*sz);
+	uint16_t *buff_1 = (uint16_t*)malloc(sizeof(uint16_t)*sz*sz);
 	for (i = 0; i < sz; i++) {
 		printf("\n");
-		buff_0[i] = (uint16_t*)malloc(sizeof(uint16_t)*sz);
-		buff_1[i] = (uint16_t*)malloc(sizeof(uint16_t)*sz);
+		// buff_0[i] = (uint16_t*)malloc(sizeof(uint16_t)*sz);
+		// buff_1[i] = (uint16_t*)malloc(sizeof(uint16_t)*sz);
 		for (j = 0; j < sz; j++) {
-			fscanf (f, "%hd", &buff_0[i][j]);
-			// fscanf (f, "%hd", &buff_0[i*sz + j]);
-			printf("%d ", buff_0[i][j]);
-			// printf("%d ", buff_0[i*sz + j]);
+			// fscanf (f, "%hd", &buff_0[i][j]);
+			fscanf (f, "%hd", &buff_0[i*sz + j]);
+			// printf("%d ", buff_0[i][j]);
+			printf("%d ", buff_0[i*sz + j]);
 		}
 	}
-	printf ("\nsizeof(buff_0) = %lu", sizeof(buff_0));
-	printf ("\nsizeof(testGrid_1) = %lu", sizeof(testGrid_1));
-	printf ("\nsizeof(buff_0[0]) = %lu", sizeof(buff_0[0]));
-	printf ("\nsizeof(testGrid_1[0]) = %lu", sizeof(buff_0[0]));
-	printf ("\naddr buff_0[0][0]) = %p", &buff_0[0][0]);
-	printf ("\naddr buff_0[sz-1][sz-1]) = %p", &buff_0[sz-1][sz-1]);
-	printf ("\naddr testGrid_1[0][0]) = %p", &testGrid_1[0][0]);
-	printf ("\naddr testGrid_1[sz-1][sz-1]) = %p", &testGrid_1[TEST_GRID_SIZE_1-1][TEST_GRID_SIZE_1-1]);
-	printf("\n[i,j]\tbuff_0\testGrid_1\tbuff_x");
-	for (i = 0; i < sz; i++) {
-		for (j = 0; j < sz; j++) {
-			printf("\n[%d][%d]\t%p ", i, j, &buff_0[i][j]);
-			printf("\t%p ", &testGrid_1[i][j]);
-			printf("\t%p ", &buff_x[i*sz +j]);
-		}
-	}
-	printf("\nlen(buff_0) = %ld", &buff_0[sz-1][sz-1] - &buff_0[0][0]);
-	printf("\nlen(buff_0 row) = %ld", &buff_0[0][sz-1] - &buff_0[0][0]);
-	printf("\nlen(buff_0 col) = %ld", &buff_0[1][0] - &buff_0[0][0]);
-	printf("\nlen(testGrid_1 row) = %ld", &testGrid_1[0][sz-1] - &testGrid_1[0][0]);
-	printf("\nlen(testGrid_1 col) = %ld", &testGrid_1[1][0] - &testGrid_1[0][0]);
-	printf("\nlen(testGrid_1) = %ld", &testGrid_1[sz-1][sz-1] - &testGrid_1[0][0]);
-	printf("\nlen(buff_0) = %ld", &buff_0[sz*sz-1] - &buff_0[0]);
+	// printf ("\nsizeof(buff_0) = %lu", sizeof(buff_0));
+	// printf ("\nsizeof(testGrid_1) = %lu", sizeof(testGrid_1));
+	// printf ("\nsizeof(buff_0[0]) = %lu", sizeof(buff_0[0]));
+	// printf ("\nsizeof(testGrid_1[0]) = %lu", sizeof(buff_0[0]));
+	// printf ("\naddr buff_0[0][0]) = %p", &buff_0[0][0]);
+	// printf ("\naddr buff_0[sz-1][sz-1]) = %p", &buff_0[sz-1][sz-1]);
+	// printf ("\naddr testGrid_1[0][0]) = %p", &testGrid_1[0][0]);
+	// printf ("\naddr testGrid_1[sz-1][sz-1]) = %p", &testGrid_1[TEST_GRID_SIZE_1-1][TEST_GRID_SIZE_1-1]);
+	// printf("\n[i,j]\tbuff_0\testGrid_1\tbuff_x");
+	// for (i = 0; i < sz; i++) {
+	// 	for (j = 0; j < sz; j++) {
+	// 		printf("\n[%d][%d]\t%p ", i, j, &buff_0[i][j]);
+	// 		printf("\t%p ", &testGrid_1[i][j]);
+	// 		printf("\t%p ", &buff_x[i*sz +j]);
+	// 	}
+	// }
+	// printf("\nlen(buff_0) = %ld", &buff_0[sz-1][sz-1] - &buff_0[0][0]);
+	// printf("\nlen(buff_0 row) = %ld", &buff_0[0][sz-1] - &buff_0[0][0]);
+	// printf("\nlen(buff_0 col) = %ld", &buff_0[1][0] - &buff_0[0][0]);
+	// printf("\nlen(testGrid_1 row) = %ld", &testGrid_1[0][sz-1] - &testGrid_1[0][0]);
+	// printf("\nlen(testGrid_1 col) = %ld", &testGrid_1[1][0] - &testGrid_1[0][0]);
+	// printf("\nlen(testGrid_1) = %ld", &testGrid_1[sz-1][sz-1] - &testGrid_1[0][0]);
+	// printf("\nlen(buff_0) = %ld", &buff_0[sz*sz-1] - &buff_0[0]);
 
 	printf("\n-----1--------------\n");
-	printGameGrid(buff_0[0], sz);
+	printGameGrid(buff_0, sz);
 	printf("\n-----2--------------\n");
-	// memset(buff_1, 1, sizeof(uint16_t)*sz*sz);
+	memset(buff_1, 0, sizeof(uint16_t)*sz*sz);
 
-	printGameGrid(buff_1[0], sz);
+	printGameGrid(buff_1, sz);
 	printf("\n-------------------\n");
 	// while (!feof (f))
 	// {  
@@ -305,19 +305,24 @@ int main(int argc, char ** argv) {
 	// 	}
 	// }
 
-	gameEntry(testGrid_1[0], emptyGrid[0], TEST_GRID_SIZE_1, 1);
+	gameEntry(buff_0, buff_1, sz, 1);
+	// gameEntry(testGrid_1[0], emptyGrid[0], TEST_GRID_SIZE_1, 1);
 	printf("\nAfter one round\n");
-	printGameGrid(emptyGrid[0], TEST_GRID_SIZE_1);
+	// printGameGrid(emptyGrid[0], TEST_GRID_SIZE_1);
+	printGameGrid(buff_1, sz);
 	// for (i = 0; i < TEST_GRID_SIZE_1; i++) {
 	// 	printf("\n");
 	// 	for (j = 0; j < TEST_GRID_SIZE_1; j++) {
 	// 		printf("%d ", emptyGrid[i][j]);
 	// 	}
 	// }
-	memset(testGrid_1, 0, sizeof(testGrid_1));
-	gameEntry(emptyGrid[0], testGrid_1[0], TEST_GRID_SIZE_1, 1);
+	// memset(testGrid_1, 0, sizeof(testGrid_1));
+	memset(buff_0, 0, sz*sz*sizeof(uint16_t));
+	// gameEntry(emptyGrid[0], testGrid_1[0], TEST_GRID_SIZE_1, 1);
+	gameEntry(buff_1, buff_0, sz, 1);
 	printf("\nAfter second round\n");
-	printGameGrid(testGrid_1[0], TEST_GRID_SIZE_1);
+	// printGameGrid(testGrid_1[0], TEST_GRID_SIZE_1);
+	printGameGrid(buff_0, sz);
 	// for (i = 0; i < TEST_GRID_SIZE_1; i++) {
 	// 	printf("\n");
 	// 	for (j = 0; j < TEST_GRID_SIZE_1; j++) {
